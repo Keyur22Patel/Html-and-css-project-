618fca6c4a486954aaf3f1e38b2c1fe8f9212afe
_Keyur Patel_ <_[keyur22p@gmail.com]_>
2022-06-06 12:07:53 -0500

    this is my

---
+++ four-card-feature-section-master/index.js
@@ -0,0 +1,15 @@
+let productName = "Mentos";
+
+let isSugerfree = true;
+
+let totalPieces = "50";
+
+let sku = "0-73390-01404-9";
+
+let manufacture;
+ const totalPieces1 = 100;
+totalPieces= 75; // 
+console.log(totalPieces);
+console.log(productName + sku);
+
+console.log(totalPieces - totalPieces1)
